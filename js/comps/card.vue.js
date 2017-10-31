0d52e47bc03af0798647c57c9e78df0c5c4c01be
Ikechi Michael <mykehell123@gmail.com>
2017-10-31 22:09:16 +0100

    renamed cards.png reference

--- js/comps/card.vue.js
+++ js/comps/card.vue.js
@@ -2,11 +2,11 @@ const Card = Vue.component('card', {
     props: ['left', 'top'],
     computed: {
         backgroundImage() {
-            return 'url(\'img/cards.png\') ' + this.left + 'px ' + this.top + 'px'
+            return 'url(\'img/Cards.png\') ' + this.left + 'px ' + this.top + 'px'
         }
     },
     template: `<div class="card" :style="{ background: backgroundImage }"></div>`,
     mounted() {
-        
+
     }
 })
